dde0a9997f6cbbc85f3bb2138d0ee025d1dc8764
rodolfo <rodolfotiago@gmail.com>
2019-07-20 13:19:17 -0300

    go

--- ConstantsCities.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    DUBLIN_ID: "City01Dublin",
-    NETHERLANDS_ID: "City02Netherlands"
-}
--- run.js
+++
@@ -1,8 +0,0 @@
-const bluebird = require('bluebird');
-global.Promise = bluebird;
-
-var test = require('./APIClients/APIFactory');
-
-
-var client = test.getInstance(require('./ConstantsCities').DUBLIN_ID);    
-client.getInformation();
