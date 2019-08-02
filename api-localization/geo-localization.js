86bbb8b3a17eb25491aa66bacd721dc5d5ed4bdc
Rodolfo Neto <rodolfotiago@gmail.com>
2019-08-02 10:01:24 -0300

    Update geo-localization.js

--- api-localization/geo-localization.js
+++ api-localization/geo-localization.js
@@ -2,7 +2,8 @@ const httpRequest = require('../helper/http-client');
 const {apis} = require('../config.json');
 
 const getLocalizationByCoordinator = async (latitude, longitude) => 
-    await httpRequest.get(`${apis.sptrans.url}/Login/Autenticar?=${apis.sptrans.token}&datalat=${latitude}&datalong=${longitude}&format=json`);
+    //await httpRequest.get(`${apis.sptrans.url}/Login/Autenticar?=${apis.sptrans.token}&datalat=${latitude}&datalong=${longitude}&format=json`);
+    await httpRequest.post(`${apis.sptrans.url}/Login/Autenticar?=${apis.sptrans.token}`);
 
 //Using Latitude and Longitude to get the current localization.
  const getLocalization = async (latitude, longitude) => {
